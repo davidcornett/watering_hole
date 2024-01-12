9d8dd067da498099bd2eb096223d51dfd93e8272
David Cornett <davidgcornett@gmail.com>
2024-01-12 22:26:35 +0200

    more content for Orgs

--- waterhole_front/src/app/page.js
+++ waterhole_front/src/app/page.js
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import SearchBar from './components/SearchBar';
 import SchoolInfo from './components/SchoolInfo';
@@ -23,6 +24,35 @@ const IntroCard = ({ title, content }) => (
 );
 
 
+/*
+const OrgCard = ({ title, content, image }) => (
+  <div style={{ margin: '20px', padding: '20px', border: '1px solid #ccc', borderRadius: '10px', backgroundColor: '#f9f9f9', textAlign: 'left',  maxWidth: '50%' }}>
+    <h2 style={{ color: '#333' }}>{title}</h2>
+    <p style={{ color: '#555' }}>{content}</p>
+    {image && <img 
+    src={image} alt={title} style={{ 
+      width: '100px', 
+      height: 'auto', 
+      marginRight: '20px'}}
+      />}
+  </div>
+);
+*/
+const OrgCard = ({ title, content, image }) => (
+  <div style={{ margin: '20px', padding: '20px', border: '1px solid #ccc', borderRadius: '10px', backgroundColor: '#f9f9f9', textAlign: 'left', maxWidth: '60%' }}>
+    <h2 style={{ color: '#333' }}>{title}</h2>
+    <p style={{ color: '#555' }}>{content}</p>
+    {image && (
+      <img 
+        src={image} 
+        alt={title} 
+        style={{ width: '100%', height: 'auto', display: 'block', marginTop: '20px' }} 
+      />
+    )}
+  </div>
+);
+
+
 const introCardsStyle = {
   display: 'flex',
   justifyContent: 'space-around',
@@ -34,6 +64,7 @@ export default function Page() {
   const [data, setData] = useState('');
   const [mapData, setMapData] = useState('');
   const [showIntro, setShowIntro] = useState(true);  // Control visibility of intro content
+  const [showOrgs, setShowOrgs] = useState(false);  // Control visibility of org content
 
   const [searchTerm, setSearchTerm] = useState('');
   const [loading, setLoading] = useState(false);
@@ -79,7 +110,6 @@ export default function Page() {
   
       setSearchTerm(term);
       setData(data);
-      console.log(data)
       setMapData(mapData);
       setShowIntro(false);
       setShowNote(data.students_change > 0 && data.students_change_sidewalk < 0);
@@ -101,6 +131,15 @@ export default function Page() {
     content: "Elite schools will fare better. This leaves even fewer graduating high school students for the rest. Non-selective schools with poor demographics will have the highest risk of financial difficulties." }
   ];
 
+  const orgCards = [
+    { title: "Find Segments of Growing Educational Clients", 
+    content: "Despite the 'demographic cliff,' many institutions will thrive due to their strong demographic pipeline and attractiveness to students. Their increasing budgets position them as strong potential clients for your organization. In contrast, other institutions will face budgetary pressures and a higher risk of financial difficulty.",
+    image: "/matrix.png"
+  },
+    { title: "Our Approach",
+    content: "We merge educational-specific datasets with demographic trends to model changes to net tuition and operating revenue for nearly 3,000 institutions." }
+  ];
+
   const isMobile = window.innerWidth <= 600;
 
   return (
@@ -108,25 +147,42 @@ export default function Page() {
     <ThemeProvider theme={theme}>
       <AppBar position="static">
         <Toolbar>
+
+          <Button
+            color="inherit"
+            onClick={() => setShowOrgs(false)} // show org content
+            sx={{ textTransform: 'none' }}
+          >
           <Typography variant="h6" style={{ flex: 1 }}>
             Search for your University
           </Typography>
-          {/*
+          </Button>
+          
+          <Button
+            color="inherit"
+            onClick={() => {
+              setShowOrgs(true);
+              setShowIntro(false);
+            }} 
+            sx={{ textTransform: 'none' }}
+          >
           <Typography variant="h6">
-            Learn more about our methodology
+            Solutions for Organizations
           </Typography>
-          */
-  }
+          </Button>
+  
         </Toolbar>
       </AppBar>
       <div>
         <div style={{ textAlign: 'center', margin: '40px 0' }}>
+
+        
+        {showIntro && (
+          <>
           <h1 style={{ fontSize: '2.5em', marginBottom: '10px' }}>Will My College Thrive?</h1>
           <p style={{ fontSize: '1.2em', maxWidth: '800px', margin: 'auto', lineHeight: '1.6', color: '#999' }}>
           Discover how changing demographics will uniquely impact each college.
           </p>
-
-          {showIntro && (
           <div style={{ 
             display: 'flex', 
             justifyContent: 'space-around', 
@@ -135,14 +191,21 @@ export default function Page() {
             }}>
             {introCards.map((card, index) => (
               <IntroCard key={index} title={card.title} content={card.content} />
+            
             ))}
           </div>
+          </>
+          )}
+        </div>
+
+        { !showOrgs && (
+        <>
+          <SearchBar onSearch={handleSearch} universityList={universityList} />
+          {loading && <p>Loading...</p>}
+          {error && <p>Error: {error.message}</p>}
+        </>
           )}
 
-        </div>
-        <SearchBar onSearch={handleSearch} universityList={universityList} />
-        {loading && <p>Loading...</p>}
-        {error && <p>Error: {error.message}</p>}
         <div style={{ 
           display: 'flex', 
           flexDfirection: 'row',
@@ -167,7 +230,39 @@ export default function Page() {
           </>
           )
           }
+
         </div>
+
+        {showOrgs && (
+          <>
+          <div style={{ textAlign: 'center', margin: '40px 0' }}>
+            <h1 style={{ fontSize: '2.5em', marginBottom: '10px' }}>Leverage Our Model to Grow Your Business</h1>
+          </div>
+
+          <div style={{ display: 'flex', justifyContent: 'space-around', margin: '20px', flexWrap: 'wrap', alignItems: 'start' }}>
+            {orgCards.map((card, index) => (
+              <OrgCard key={index} title={card.title} content={card.content} image={card.image} />
+            ))}
+          </div>
+
+
+          {/*
+          <div style={{ 
+            display: 'flex', 
+            justifyContent: 'space-around', 
+            margin: '20px',
+            flexWrap: isMobile ? 'wrap' : 'nowrap' // vertically align cards on mobile
+            }}>
+            {orgCards.map((card, index) => (
+              <OrgCard key={index} title={card.title} content={card.content} image={card.image}/>
+            ))}
+          </div>
+          */}
+          </>
+          )}
+
+
+
       </div>
 
     </ThemeProvider>
